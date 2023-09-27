d3544d6da81c0c63a9f08e7a370e226c7f1ecc9c
farshid karimi <karimi_f@yaftar.ir>
2023-09-27 13:34:52 +0330

    layout+page+footer+navbar

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      {children}
+      BlogLayout
+    </div>
+  );
+}
