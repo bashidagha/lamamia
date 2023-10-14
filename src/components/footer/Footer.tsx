486f601281a8f1b7320d0dda3399230565718943
farshid karimi <karimi_f@yaftar.ir>
2023-10-14 12:31:07 +0330

    add footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import styles from './footer.module.css'
+import Image from 'next/image';
+import React from 'react';
+import styles from './footer.module.css';
 
 export default function Footer() {
-  return (
-    <div className={styles.container}>
-        <div>©2023 Lamamia. All rights reserved.</div>
-        <div></div>
-
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <div>©2023 Lamamia. All rights reserved.</div>
+            <div className={styles.social}>
+                <Image
+                    src='/1.png'
+                    alt='Lama Dev'
+                    className={styles.icon}
+                    width={15}
+                    height={15}
+                />
+                <Image
+                    src='/2.png'
+                    alt='Lama Dev'
+                    className={styles.icon}
+                    width={15}
+                    height={15}
+                />
+                <Image
+                    src='/3.png'
+                    alt='Lama Dev'
+                    className={styles.icon}
+                    width={15}
+                    height={15}
+                />
+                <Image
+                    src='/4.png'
+                    alt='Lama Dev'
+                    className={styles.icon}
+                    width={15}
+                    height={15}
+                />
+            </div>
+        </div>
+    );
 }
