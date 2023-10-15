07dd918692b9ac94827be66550df16d89a16032c
farshid karimi <karimi_f@yaftar.ir>
2023-10-15 16:26:16 +0330

    portofolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import styles from './page.module.css';
 
 export default function Portfolio() {
-  return (
-    <div>Portfolio</div>
-  )
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Our Works</h1>
+            <div className={styles.content}>
+                <p className={styles.choose}>Choose a gallery</p>
+                <div className={styles.items}>
+                    <div className={styles.item}>
+                        <Image
+                            alt=''
+                            src='/illustration.png'
+                            fill={true}
+                            className={styles.img}
+                        />
+                        <p className={styles.text}>Illustration</p>
+                    </div>
+                    <div className={styles.item}>
+                        <Image
+                            alt=''
+                            src='/websites.jpg'
+                            fill={true}
+                            className={styles.img}
+                        />
+                        <p className={styles.text}>Websites</p>
+                    </div>
+                    <div className={styles.item}>
+                        <Image
+                            alt=''
+                            src='/apps.jpg'
+                            fill={true}
+                            className={styles.img}
+                        />
+                        <p className={styles.text}>Applications</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
