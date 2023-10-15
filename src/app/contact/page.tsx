511a2fd56f55d2a06de41ade83e3de81e15b3c66
farshid karimi <karimi_f@yaftar.ir>
2023-10-15 15:49:04 +0330

    contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,24 +1,40 @@
 import Image from 'next/image';
 import React from 'react';
 import styles from './page.module.css';
+import Button from '../../components/Button/Butoon';
 
 export default function Contact() {
     return (
         <div className={styles.container}>
-            <div className={styles.titleContainer}>
-                <h1>Let&apos;s Keep in Touch</h1>
-            </div>
-
-            <div className={styles.mainContainer}>
+            <h1 className={styles.title}>Let's Keep in Touch</h1>
+            <div className={styles.content}>
                 <div className={styles.imgContainer}>
                     <Image
-                        fill={true}
-                        alt=''
                         src='/contact.png'
-                        className={styles.img}
+                        alt=''
+                        fill={true}
+                        className={styles.image}
                     />
                 </div>
-                <div className={styles.formContainer}></div>
+                <form className={styles.form}>
+                    <input
+                        type='text'
+                        placeholder='name'
+                        className={styles.input}
+                    />
+                    <input
+                        type='text'
+                        placeholder='email'
+                        className={styles.input}
+                    />
+                    <textarea
+                        className={styles.textArea}
+                        placeholder='message'
+                        cols='30'
+                        rows='10'
+                    ></textarea>
+                    <Button url='#' text='Send' />
+                </form>
             </div>
         </div>
     );
